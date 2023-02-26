d69b82db5078097ad62b233487008d863aab6a68
Oleksii428 <wifi5324518@gmail.com>
2023-02-27 00:30:11 +0200

    frontend: title filter

--- frontend/src/components/TitleFilter/TitleFilter.tsx
+++ frontend/src/components/TitleFilter/TitleFilter.tsx
@@ -1,28 +1,46 @@
-import React, {FC} from "react";
+import {ChangeEvent, FC, useEffect, useState} from "react";
 import {TextField} from "@mui/material";
-import {useSearchParams} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useAppLocation} from "../../hooks";
 
 const TitleFilter: FC = () => {
-	const [searchParams, setSearchParams] = useSearchParams({});
+	const location = useAppLocation();
+	const navigate = useNavigate();
+	const [searchParams, setSearchParams] = useState<URLSearchParams>(new URLSearchParams(location.search));
+	const [currValue, setCurrValue] = useState<string>(searchParams.get("title") ?? "");
 
+	useEffect(() => {
+		console.log("g");
+		setSearchParams(new URLSearchParams(location.search));
+	}, [location.search]);
+
+	useEffect(() => {
+		console.log("f");
+		setCurrValue(searchParams.get("title") ?? "");
+	}, [searchParams]);
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.currentTarget.value;
+		if (value) {
+			searchParams.set("title", value);
+			navigate({search: searchParams.toString()});
+			setCurrValue(value);
+		} else {
+			searchParams.delete("title");
+			navigate({search: searchParams.toString()});
+			setCurrValue("");
+		}
+		setSearchParams(searchParams);
+	};
 
 	return (
 		<TextField
 			id="standard-basic"
 			label="Title"
-			value={sessionStorage.getItem("title")}
+			value={currValue}
 			variant="standard"
-			onChange={(event) => {
-				const value = event.target.value;
-				if (value) {
-					sessionStorage.setItem("title", value);
-					setSearchParams((prev) => ({...prev, title: value}));
-				} else {
-					sessionStorage.removeItem("title");
-					searchParams.delete("title");
-					setSearchParams(searchParams);
-				}
-			}} />
+			onChange={handleChange}
+		/>
 	);
 };
 
