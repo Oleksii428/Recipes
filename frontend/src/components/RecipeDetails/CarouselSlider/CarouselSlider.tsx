26b0542e3fb3beaaf5b5d35afb4d9bbe832d9639
Oleksii428 <wifi5324518@gmail.com>
2023-03-03 17:22:07 +0200

    frontend: recipe details (carousel slider add video)

--- frontend/src/components/RecipeDetails/CarouselSlider/CarouselSlider.tsx
+++ frontend/src/components/RecipeDetails/CarouselSlider/CarouselSlider.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import Carousel from "react-material-ui-carousel";
 import {Box, Paper} from "@mui/material";
+import {ArrowForwardIosRounded, ArrowBackIosRounded} from "@mui/icons-material";
 
 import {baseURL} from "../../../configs";
 
@@ -16,36 +17,72 @@ interface IProps {
 const CarouselSlider: FC<IProps> = ({gallery}) => {
 	return (
 		<Carousel
+			NextIcon={<ArrowForwardIosRounded />}
+			PrevIcon={<ArrowBackIosRounded />}
+			navButtonsProps={{
+				style: {
+					backgroundColor: "#1976d2"
+				}
+			}}
+			activeIndicatorIconButtonProps={{
+				style: {
+					color: "#1976d2"
+				}
+			}}
 			height={300}
 			animation="slide"
 			stopAutoPlayOnHover={true}
 			navButtonsAlwaysVisible={true}
+			fullHeightHover={false}
 			indicators={true}
+			autoPlay={false}
 		>
-			{
-				gallery.map((item, i) =>
+			{gallery.map(({_id, path}) => {
+				const fileExt = path.split(".").pop();
+				const isVideo = fileExt === "mp4" || fileExt === "webm";
+
+				return (
 					<Paper
-						key={i}
+						key={_id}
 						elevation={0}
 						sx={{
 							height: 300,
 							position: "relative"
-						}}>
-						<Box
-							component="img"
-							src={baseURL + item.path}
-							sx={{
-								width: "100%",
-								height: "100%",
-								position: "absolute",
-								objectFit: "contain",
-								top: 0,
-								left: 0
-							}}
-						/>
+						}}
+					>
+						{isVideo ? (
+							<Box
+								component="video"
+								src={`${baseURL}${path}`}
+								controls={true}
+								loop={true}
+								muted={true}
+								sx={{
+									width: "100%",
+									height: "100%",
+									position: "absolute",
+									objectFit: "contain",
+									top: 0,
+									left: 0
+								}}
+							/>
+						) : (
+							<Box
+								component="img"
+								src={`${baseURL}${path}`}
+								sx={{
+									width: "100%",
+									height: "100%",
+									position: "absolute",
+									objectFit: "contain",
+									top: 0,
+									left: 0
+								}}
+							/>
+						)}
 					</Paper>
-				)
-			}
+				);
+			})}
 		</Carousel>
 	);
 };
