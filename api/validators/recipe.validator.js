38c74ccac6d27b40c812f0ce1684ccf21e4b59e9
Oleksii428 <wifi5324518@gmail.com>
2023-03-20 15:22:58 +0200

    frontend: fix refresh cycle; change validators

--- api/validators/recipe.validator.js
+++ api/validators/recipe.validator.js
@@ -4,23 +4,23 @@ const {regex} = require("../enums");
 
 module.exports = {
 	createRecipeValidator: Joi.object({
-		title: Joi.string().trim().min(2).max(20).required().lowercase(),
+		title: Joi.string().trim().min(2).max(40).required().lowercase(),
 		time: Joi.number().integer().min(1).max(500).required(),
 		servings: Joi.number().min(1).max(50).required(),
-		description: Joi.string().min(10).max(300).required(),
+		description: Joi.string().min(10).max(1000).required(),
 		category: Joi.string().regex(regex.MONGO_ID).required(),
 		kitchen: Joi.string().regex(regex.MONGO_ID).required(),
-		ingredients: Joi.array().items(Joi.string().min(3).max(20)).min(1).required(),
+		ingredients: Joi.array().items(Joi.string().min(3).max(40)).min(1).required(),
 		rating: Joi.number().min(0).max(5).default(0),
 		creator: Joi.string().regex(regex.MONGO_ID).optional(),
 		isModerated: Joi.boolean().default(false)
 	}),
 	updateRecipeValidator: Joi.object({
-		title: Joi.string().trim().lowercase().min(2).max(20).required(),
+		title: Joi.string().trim().lowercase().min(2).max(40).required(),
 		time: Joi.number().min(1).max(500).required(),
 		servings: Joi.number().min(1).max(50).required(),
-		description: Joi.string().min(10).max(300).required(),
-		ingredients: Joi.array().items(Joi.string().min(3).max(20)).min(1).required(),
+		description: Joi.string().min(10).max(1000).required(),
+		ingredients: Joi.array().items(Joi.string().min(3).max(40)).min(1).required(),
 		isModerated: Joi.boolean().default(false)
 	})
 };
--- api/validators/stage.validator.js
+++ api/validators/stage.validator.js
@@ -7,6 +7,6 @@ module.exports = {
 		recipe: Joi.string().regex(regex.MONGO_ID).required(),
 		number: Joi.number().min(1).max(30).required().default(1),
 		photo: Joi.string().regex(regex.MONGO_ID).optional().default(null),
-		description: Joi.string().min(5).max(200).required()
+		description: Joi.string().min(5).max(1000).required()
 	})
 };
